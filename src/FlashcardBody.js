2da6c2d321331a69824d821b8d009fb0260f99c9
Taylor-Bracone <71147213+Taylor-Bracone@users.noreply.github.com>
2021-11-11 14:24:13 -0500

    minor update

--- src/FlashcardBody.js
+++ src/FlashcardBody.js
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { I18NMixin } from '@lrnwebcomponents/i18n-manager/lib/I18NMixin.js';
 
-export class BtoProBox extends I18NMixin(LitElement) {
+export class FlashcardBody extends I18NMixin(LitElement) {
     static get tag() {
       return 'FlashcardBody,js';
     }
