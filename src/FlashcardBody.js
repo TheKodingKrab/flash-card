7cc6f85e56a474e0a914c763f2db27776f413444
ejk1220 <ejkennedy137@gmail.com>
2021-12-02 13:50:34 -0500

    added new languages in locales

--- src/FlashcardBody.js
+++ src/FlashcardBody.js
@@ -23,8 +23,12 @@ export class FlashcardBody extends I18NMixin(SimpleColors) {
     this.registerLocalization({
       context: this,
       localesPath: new URL('../locales/', import.meta.url).href,
-      locales: ['es'],
+      locales: ['es', 'fr', 'de'],
     });
+    this.speech = new SpeechSynthesisUtterance();
+    this.speech.lang = navigator.language.substring(0, 2); // uses language of the browser
+    this.i18store = window.I18NManagerStore.requestAvailability();
+    this.speech.lang = this.i18store.lang;
   }
 
   static get properties() {
